90741197b0fba98bb1ca988c43b633964e42a5e7
Baptiste Saez <baptiste.saez@etu.univ-grenoble-alpes.fr>
2025-03-19 09:08:49 +0100

    rename tests

--- src/test/java/fr/project/lib/DataFrameTest.java
+++ src/test/java/fr/project/lib/DataFrameTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class DataFrame {
-
+public class DataFrameTest {
+    @Test
+    void create() {
+        var v = new DataFrame();
+    }
 }
