2a3c5a49ed3779ec62b471eb572a32eb4d5726cc
Baptiste Saez <baptiste.saez@etu.univ-grenoble-alpes.fr>
2025-03-19 08:55:28 +0100

    init maven

---
+++ src/main/java/fr/project/lib/DataFrame.java
@@ -0,0 +1,8 @@
+package fr.project.lib;
+
+public class DataFrame<T> {
+    T[][] data;
+    String[] col_label;
+    String[] li_label;
+
+}
---
+++ src/test/java/fr/project/lib/DataFrame.java
@@ -0,0 +1,9 @@
+package fr.project.lib;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DataFrame {
+
+}
