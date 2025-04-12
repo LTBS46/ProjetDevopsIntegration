804961fc0706a0870442d4934d376480f63f32d5
Baptiste Saez <baptiste.saez@etu.univ-grenoble-alpes.fr>
2025-04-12 23:54:41 +0200

    fix version_state_checker

--- version_state_checker.py
+++ version_state_checker.py
@@ -1,9 +1,13 @@
 from subprocess import run
 
-commit = run(
-    ["git", "log", "--name-status", "HEAD^..HEAD"],
-    capture_output=True, check=True
-).stdout.decode("UTF-8").split("\n")
+#commit = run(
+#    ["git", "log", "--name-status", "HEAD^..HEAD"],
+#    capture_output=True, check=True
+#).stdout.decode("UTF-8").split("\n")
+from sys import stdin
+
+commit = stdin.read().split("\n")
+
 print(commit)
 header, author, _date, b1, message, b2, *modifications, b3 = commit
 print(header)
